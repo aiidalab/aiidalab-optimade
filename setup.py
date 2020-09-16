f84953142bda5af19908fbc778bf5c4282a39e0d
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-09-16 18:06:28 +0200

    Add dependabot v2
    Put dependencies into `requirements.txt`, supporting development
    versions.
    Use dependabot v2 to make sure the version is updated when needed.
    
    Add .gitignore with standard Python 3 entries.
    
    Also, add author_email to setup().

--- setup.py
+++ setup.py
@@ -8,13 +8,17 @@ TOP_DIR = Path(__file__).parent.resolve()
 with open(TOP_DIR.joinpath("metadata.json")) as handle:
     METADATA = json.load(handle)
 
+with open(TOP_DIR.joinpath("requirements.txt")) as handle:
+    REQUIREMENTS = handle.read()
+
 setup(
     name="aiidalab-optimade",
     version=METADATA["version"],
     license="MIT License",
     author="AiiDAlab Team",
+    author_email="aiidalab@materialscloud.org",
     description=METADATA["description"],
     url="https://github.com/aiidalab/aiidalab-optimade",
     packages=find_packages(),
-    install_requires=["optimade-client~=2020.9"],
+    install_requires=REQUIREMENTS,
 )
